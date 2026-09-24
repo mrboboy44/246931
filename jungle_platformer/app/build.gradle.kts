d06f75dfe23383d701b3842721ef019a07ee5675
mrboboy44 <ilhamahriansyah@gmail.com>
2026-09-24 19:54:19 +0700

    Add native platformer project

---
+++ jungle_platformer/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+android {
+    namespace = "com.example.jungleplatformer"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.example.jungleplatformer"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
